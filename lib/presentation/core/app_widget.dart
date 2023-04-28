f09140264fbcee96ff5d93f3c1ef5635b9e8cb7b
xJac0b <kubap1402@gmail.com>
2023-04-28 14:36:56 +0200

    l10n

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import '../../utils/extensions.dart';
 import 'router/router.dart';
 
 class MyAppWidget extends StatelessWidget {
@@ -9,8 +11,32 @@ class MyAppWidget extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: _appRouter.config(),
-      title: '',
-      theme: ThemeData(useMaterial3: true),
+      title: 'My Blood',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('pl'),
+      ],
+      theme: ThemeData(
+        useMaterial3: true,
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+          headlineMedium: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
