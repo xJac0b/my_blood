590e1f93d66c65e864612802fe3ee360dedf41c9
xJac0b <kubap1402@gmail.com>
2023-04-28 14:37:18 +0200

    context extensions

---
+++ lib/utils/extensions.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+extension AppLocalizationsX on BuildContext {
+  AppLocalizations get l10n => AppLocalizations.of(this);
+}
+
+extension MediaQueryX on BuildContext {
+  Size get screenSize => MediaQuery.of(this).size;
+}
+
+extension ThemeColorX on BuildContext {
+  ColorScheme get colors => Theme.of(this).colorScheme;
+}
