341f6ace34b27a4da71d6cd76d98e8994ed6cafa
xJac0b <kubap1402@gmail.com>
2023-05-01 21:28:25 +0200

    trying fix workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'presentation/core/my_app_widget.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyAppWidget());
 }
+
--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -4,7 +4,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this);
+  // ignore: unnecessary_cast
+  AppLocalizations get l10n => AppLocalizations.of(this) as AppLocalizations;
 }
 
 extension MediaQueryX on BuildContext {
